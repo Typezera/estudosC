dbaa2d3635460a0b268e69bb1649f1e98176adb8
Typezera <willianbardela2131@hotmail.com>
2026-05-07 20:50:28 -0300

    feat: capturando a entrada do usuário para alocar um array diâmico na heap

--- gerenciadorTarefas/funcoes.c
+++ gerenciadorTarefas/funcoes.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 
 void mostrarMenu(){
     printf("\n=======MENU=======\n");
--- gerenciadorTarefas/main.c
+++ gerenciadorTarefas/main.c
@@ -1,49 +1,49 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 //importe da struct tarefa + enum
 #include "structs/tarefas.h"
 #include "funcoes.h"
 
+
 int main() {
-    // Tarefas t;
-
-    // strcpy(t.nome, "limpar a casa");
-    // strcpy(t.descricao, "Preciso lavar o banheiro");
-    // t.estado = PENDENTE;
-
-    // criarTarefa(&t);
-
-    // Tarefas lista_tarefas[10];
     char buffer_choice[5];
-
+    char buffer_quanTarefas[3];
+    int tot;
+    
     printf("Bem vindo ao Organizer.\n");
+    do
+    {
+        printf("Informe quantas tarefas serão cadastradas? ");
+        if (fgets(buffer_quanTarefas, sizeof(buffer_quanTarefas), stdin) == NULL)
+        {
+            printf("[ERROR] Ao passar a quantidade.");
+        };
+        tot = strtol(buffer_quanTarefas, NULL, 10);
+    } while (buffer_quanTarefas[0] == '\n');
+    printf("%d", tot);
+    
+    // if(fgets(buffer_quanTarefas, sizeof(buffer_quanTarefas), stdin) == NULL){
+    //     printf("[ERROR] Ao passar a quantidade.");
+    // };
+    // while (buffer_quanTarefas[0] == "\n")
+    // {
+    //     if(fgets(buffer_quanTarefas, sizeof(buffer_quanTarefas), stdin) == NULL){
+    //         printf("[ERROR] Ao passar a quantidade.");
+    //     };
+    // }
+
     printf("Escolha uma das opções do menu\n");
     mostrarMenu();
+    printf("\n");
+
 
     while(1){
         fgets(buffer_choice, sizeof(buffer_choice), stdin);
         escolhaMenu(buffer_choice);
-        
-        // printf("insira o nome da tarefa: ");
-        // fgets(buffer_name, sizeof(buffer_name), stdin);
-        // printf("\n");
-    
-        // printf("insira uma decrição da tarefa: ");
-        // fgets(buffer_descricao, sizeof(buffer_descricao), stdin);
-        // printf("%s", buffer_descricao);
-
         
     }
 
 
     return 0;
 }
-// Tarefas t;
-
-// strcpy(t.nome, "Limpar a casa");
-// strcpy(t.descricao, "Pegar um dia da semana para fazer uma faxina na casa");
-// t.estado = PENDENTE;
-
-// printf("nome: %s\n", t.nome);
-// printf("descricao: %s\n", t.descricao);
-// printf("status: %d\n", t.estado);
